c9308df7fd6d4c131019cd7868de0afa89865fd8
jmitt-redhat <102777124+jmitt-redhat@users.noreply.github.com>
2024-07-21 13:58:24 +1000

    Initial fork

--- src/windows/win-crowdstrike-fix-bootloop.ps1
+++ src/windows/win-crowdstrike-fix-bootloop.ps1
@@ -1,5 +1,54 @@
-. .\src\windows\common\setup\init.ps1
-. .\src\windows\common\helpers\Get-Disk-Partitions.ps1
+### SETUP FOR LOGGING ###
+Function Log-Output
+{
+	Param([Parameter(Mandatory=$true)][PSObject[]]$message)
+	Write-Output "[Output $(Get-Date)]$message"
+}
+Function Log-Info
+{
+	Param([Parameter(Mandatory=$true)][PSObject[]]$message)
+	Write-Output "[Info $(Get-Date)]$message"
+}
+Function Log-Warning
+{
+	Param([Parameter(Mandatory=$true)][PSObject[]]$message)
+	Write-Output "[Warning $(Get-Date)]$message"
+}
+Function Log-Error
+{
+	Param([Parameter(Mandatory=$true)][PSObject[]]$message)
+	Write-Output "[Error $(Get-Date)]$message"
+}
+Function Log-Debug
+{
+	Param([Parameter(Mandatory=$true)][PSObject[]]$message)
+	Write-Output "[Debug $(Get-Date)]$message"
+}
+		
+
+$STATUS_SUCCESS = '[STATUS]::SUCCESS'
+$STATUS_ERROR = '[STATUS]::ERROR'
+###
+
+### Add Get-Disk-Partitions function for KVM ###
+function Get-Disk-Partitions()
+{
+	$partitionlist = $null
+	$disklist = get-wmiobject Win32_diskdrive |Where-Object {$_.model -like 'Microsoft Virtual Disk'} 
+	ForEach ($disk in $disklist)
+	{
+		$diskID = $disk.index
+		$command = @"
+		select disk $diskID
+		online disk noerr
+"@
+		$command | diskpart | out-null
+
+		$partitionlist += Get-Partition -DiskNumber $diskID
+	}
+	return $partitionlist
+}
+###
 
 $partitionlist = Get-Disk-Partitions
 $actionTaken = $false
